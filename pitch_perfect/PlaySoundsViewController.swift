053ea6267d4ac6592376837aa13922109c2eec48
Jonathan Sayer <jon.sayer1@gmail.com>
2016-06-10 16:55:31 +0100

    bug fixes

--- pitch_perfect/PlaySoundsViewController.swift
+++ pitch_perfect/PlaySoundsViewController.swift
@@ -6,17 +6,28 @@
 //  Copyright © 2016 Rawnet. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    @IBOutlet weak var snailButton: UIButton!
-    @IBOutlet weak var chipmunkButton: UIButton!
     @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var darthVaderButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
-    @IBOutlet weak var reverblButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
     
     @IBAction func playSoundForButton(sender: UIButton){
         print("Play Sound Button Pressed")
@@ -25,8 +36,6 @@ class PlaySoundsViewController: UIViewController {
     @IBAction func stopButtonPressed(sender: AnyObject){
         print("Stop Audio Button Pressed")
     }
-    
-    var recordedAudioURL: NSURL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
