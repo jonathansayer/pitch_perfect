017ffc015e0c7375a6b6c85e861ef61e32e10dbd
Jonathan Sayer <jon.sayer1@gmail.com>
2016-06-16 08:56:05 +0100

    can play back recordings

--- pitch_perfect/PlaySoundsViewController.swift
+++ pitch_perfect/PlaySoundsViewController.swift
@@ -29,12 +29,28 @@ class PlaySoundsViewController: UIViewController {
     
     enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
     
-    @IBAction func playSoundForButton(sender: UIButton){
+    @IBAction func playSoundForButton(sender: UIButton) {
         print("Play Sound Button Pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate:0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.Playing)
     }
     
-    @IBAction func stopButtonPressed(sender: AnyObject){
+    @IBAction func stopButtonPressed(sender: AnyObject) {
         print("Stop Audio Button Pressed")
+        stopAudio()
     }
 
     override func viewDidLoad() {
